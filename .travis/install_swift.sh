826518152cdf2232407e43d7bab65c3bc680a2f8
Danny Sung <danny@dannysung.com>
2020-07-25 09:10:35 -0700

    travis: debug CI; README: added SPM and Swift badges

--- .travis/install_swift.sh
+++ .travis/install_swift.sh
@@ -12,6 +12,11 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     echo "export PATH=\"${PWD}/${SWIFT_VERSION}-RELEASE-ubuntu18.04/usr/bin:\${PATH}\"" >> ~/.bashrc
 	cd "$DIR"
     swift -version
+    echo "* Home Folder"
+    ls -lFa ~/
+    echo "* Environment"
+    env
+    echo "* Done install"
 else
 	#export SWIFT_VERSION=swift-3.1.1-RELEASE
     #curl -O https://swift.org/builds/swift-3.1.1-release/xcode/${SWIFT_VERSION}/${SWIFT_VERSION}-osx.pkg
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 [![Build Status](https://travis-ci.org/dannys42/SwiftHexFormatter.svg?branch=master)](https://travis-ci.org/dannys42/SwiftHexFormatter)
+![Swift 5.1](https://img.shields.io/badge/Swift-5.1-orange.svg)
+[![Swift Package Manager Compatible](https://img.shields.io/badge/spm-compatible-brightgreen.svg)](https://swift.org/package-manager)
+
 
 # HexFormatter
 
