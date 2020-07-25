53bfa34a6095942bd24835e55372b9500d1e55de
Danny Sung <danny@dannysung.com>
2020-07-25 08:45:40 -0700

    Added travis rules

---
+++ scripts/install_swift.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ref: https://owensd.io/2017/04/28/travis-ci-swift-oslinux-config/
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+	DIR="$(pwd)"
+	cd ..
+	export SWIFT_VERSION=swift-5.2.4
+    wget https://swift.org/builds/${SWIFT_VERSION}-release/ubuntu1804/${SWIFT_VERSION}-RELEASE/${SWIFT_VERSION}-RELEASE-ubuntu18.04.tar.gz
+	tar xzf $SWIFT_VERSION-ubuntu18.04.tar.gz
+	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu18.04/usr/bin:${PATH}"
+	cd "$DIR"
+else
+	#export SWIFT_VERSION=swift-3.1.1-RELEASE
+    #curl -O https://swift.org/builds/swift-3.1.1-release/xcode/${SWIFT_VERSION}/${SWIFT_VERSION}-osx.pkg
+	#sudo installer -pkg ${SWIFT_VERSION}-osx.pkg -target /
+	#export TOOLCHAINS=swift
+    echo "TODO: Not available"
+    env
+    false
+fi
