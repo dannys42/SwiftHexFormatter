8c124369ae66c0150710f52763e1217c421b10c0
Danny Sung <danny@dannysung.com>
2024-10-24 11:16:12 -0700

    Renamed SimpleHexFormat to AttributedSimpleHexByteFormat; Added convenience SimpleHexFormat for non-attributed Strings

--- Sources/HexFormatter/SimpleHexFormat.swift
+++ Sources/HexFormatter/SimpleHexFormat.swift
@@ -12,84 +12,29 @@ public struct SimpleHexByteFormat: FormatStyle {
 //    public typealias FormatInput = ContiguousBytes & Sequence
     public typealias FormatInput = Data
 
-    public typealias FormatOutput = AttributedString
+    public typealias FormatOutput = String
     
-    public enum Style {
-        case none
-        case cStylePrefix
-        case hSuffix
-    }
+    let attributedFormatter: AttributedSimpleHexByteFormat
     
-    let prefix: AttributedString?
-    let suffix: AttributedString?
-    let byteSeparator: AttributedString?
-    
-    init(prefix: AttributedString?=nil, suffix: AttributedString?=nil, byteSeparator: AttributedString?=nil) {
-        self.prefix = prefix
-        self.suffix = suffix
-        self.byteSeparator = byteSeparator
-    }
     init(prefix: String?=nil, suffix: String?=nil, byteSeparator: String?=nil) {
-        if let prefix {
-            self.prefix = AttributedString(prefix)
-        } else {
-            self.prefix = nil
-        }
-        if let suffix {
-            self.suffix = AttributedString(suffix)
-        } else {
-            self.suffix = nil
-        }
-        if let byteSeparator {
-            self.byteSeparator = AttributedString(byteSeparator)
-        } else {
-            self.byteSeparator = nil
-        }
+        self.attributedFormatter = AttributedSimpleHexByteFormat(prefix: prefix, suffix: suffix, byteSeparator: byteSeparator)
     }
     
-    init() {
-        self.prefix = nil
-        self.suffix = nil
-        self.byteSeparator = nil
-    }
-
-    init(style: Style) {
-        switch style {
-        case .none:
-            self.init()
-        case .cStylePrefix:
-            self.init(prefix: "0x")
-        case .hSuffix:
-            self.init(suffix: "h")
-        }
+    init(style: AttributedSimpleHexByteFormat.Style) {
+        self.attributedFormatter = AttributedSimpleHexByteFormat(style: style)
     }
     
-    public func format(_ value: FormatInput) -> AttributedString {
+    public func format(_ value: FormatInput) -> String {
+        let aString = self.attributedFormatter.format(value)
         
-        value.withUnsafeBytes { bufferPointer in
-            let emptyString = AttributedString()
-            
-            var aString = prefix ?? emptyString
-            
-            let data = Data(bufferPointer)
-            
-            for (index, byte) in data.enumerated() {
-                let hexByte = String(format: "%02x", byte)
-                aString += AttributedString(hexByte)
-                
-                if let byteSeparator,
-                    index < bufferPointer.count {
-                    aString += byteSeparator
-                }
-            }
-
-            return aString + (suffix ?? emptyString)
-        }
+        let string = String(aString.characters)
+        
+        return string
     }
 }
 
-extension FormatStyle where Self == SimpleHexByteFormat {
-    public static func simpleHexByteFormat(style: SimpleHexByteFormat.Style = .none) -> SimpleHexByteFormat {
+extension FormatStyle where Self == AttributedSimpleHexByteFormat {
+    public static func simpleHexByteFormat(style: AttributedSimpleHexByteFormat.Style = .none) -> SimpleHexByteFormat {
         
         SimpleHexByteFormat(style: style)
     }
